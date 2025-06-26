07a15be5570d8ab28d176dfba08e98b372d6d018
Enpewww <nadyanputra10@gmail.com>
2025-06-26 09:43:56 +0700

    update

--- src/prediction.py
+++ src/prediction.py
@@ -28,9 +28,9 @@ def run():
             order_id = st.number_input("Order ID", min_value=1, value=1000, step=1)
             order_date = st.text_input('Order Date (mm/dd/yyyy)', value='mm/dd/yyyy')
             customer_name = st.text_input('Customer Name', value=None)
-            city = st.text_input('City', value=None)
+            city = st.text_input('City', value='Groningen')
             country = st.selectbox('Country', ['Sweden', 'United Kingdom', 'France', 'Italy', 'Austria', 'Spain', 'Germany', 'Netherlands', 'Denmark', 'Belgium', 'Norway', 'Portugal', 'Switzerland', 'Ireland', 'Finland'])
-            state = st.text_input("State", value=None)
+            state = st.selectbox("State", ["Stockholm", "England", "Auvergne-Rhône-Alpes", "Provence-Alpes-Côte d'Azur", "Languedoc-Roussillon-Midi-Pyrénées", "Liguria", "Vienna", "Murcia", "Lower Saxony", "South Holland", "Västra Götaland", "Hovedstaden", "Valenciana", "South Denmark", "Lombardy", "Sicily", "Ile-de-France", "North Rhine-Westphalia", "Flemish Brabant", "Tuscany", "Emilia-Romagna", "Madrid", "Oslo", "Lisboa", "Saxony", "Andalusía", "Catalonia", "Alsace-Champagne-Ardenne-Lorraine", "Bavaria", "Uppsala", "Nord-Pas-de-Calais-Picardie", "Hesse", "Overijssel", "Basel-Stadt", "Bourgogne-Franche-Comté", "Zürich", "Dublin", "Lazio", "Namur", "North Holland", "Berlin", "Baden-Württemberg", "Aquitaine-Limousin-Poitou-Charentes", "Uusimaa", "Apulia", "Saxony-Anhalt", "Rogaland", "Sardinia", "Drenthe", "Mecklenburg-Vorpommern", "North Brabant", "Umbria", "Geneva", "Veneto", "Normandy", "Scotland", "Coimbra", "Castile and León", "Gelderland", "Hamburg", "Brandenburg", "Pays de la Loire", "Antwerp", "Bremen", "Thuringia", "Porto", "Utrecht", "Castile-La Mancha", "Brittany", "Campania", "Cork", "Groningen", "East Flanders", "Ceuta", "Halland", "Navarra", "Rhineland-Palatinate", "Limburg", "Upper Austria", "Schleswig-Holstein", "Tyrol", "Corsica", "Trentino-Alto Adige", "Vaud", "Piedmont", "Calabria", "Galicia", "Buskerud", "Centre-Val de Loire", "Styria", "Abruzzi", "Basque Country", "Cantabria", "Asturias", "Finland Proper", "Central Jutland", "Wales", "Kymenlaakso", "Friesland", "Braga", "Aveiro", "Marche", "Saarland", "Skåne", "Friuli-Venezia Giulia", "Balearic Islands", "Extremadura", "Basilicata", "Hedmark", "Hainaut", "Melilla", "Salzburg", "Carinthia", "Zeeland", "Hordaland", "Lucerne", "Liège", "West Flanders", "Bern", "Brussels", "Södermanland", "Zealand", "Galway", "Värmland", "Vest-Agder", "St. Gallen", "Setúbal"])
             region = st.selectbox("Region", ['Central', 'South', 'North'])
 
         with col2:
@@ -38,7 +38,7 @@ def run():
             category = st.selectbox("Category", ['Office Supplies', 'Furniture', 'Technology'])
             ship_mode = st.selectbox("Ship Mode", ['Economy Plus', 'Immediate', 'Economy', 'Priority'])
             sub_category = st.selectbox("Sub-Category", ['Paper', 'Bookcases', 'Art', 'Binders', 'Tables', 'Chairs', 'Appliances', 'Labels', 'Fasteners', 'Envelopes', 'Accessories', 'Furnishings', 'Copiers', 'Storage', 'Supplies', 'Phones', 'Machines'])
-            product_name = st.text_input("Product Name", value=None)
+            product_name = st.text_input("Product Name", value='Ikea Classic Bookcase, Metal')
             quantity = st.number_input("Quantity", min_value=1, value=2, step=1)
             cost = st.number_input("Cost", min_value=0.0, value=28.0, step=0.1)
             sales = st.number_input("Sales", min_value=0.0, value=30.0, step=0.1)
